e8271aa5e743a689ae2e637ce91481aafebccf61
David <davidzah6@seznam.cz>
2021-07-13 20:41:22 +0200

    lcd fix

--- lcd_driver.py
+++ lcd_driver.py
@@ -1,16 +1,24 @@
-import pylcdlib
+from RPLCD import i2c
 import time
 import sys
 
 class Display:
     def __init__(self, addres = 0x27):
-        self.lcd = pylcdlib.lcd(addres,1)
-        self.lcd.lcd_write(0x01)
-        self.lcd.lcd_puts(str('Nvias'), 1)
+        #TODO opravit nastavování
+        lcdmode = 'i2c'
+        cols = 16
+        rows = 2
+        charmap = 'A00'
+        i2c_expander = 'PCF8574'
+        address = 0x27
+        port = 1
+
+        self.lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
+                          cols=cols, rows=rows)
 
 
     def write(self,text):
-        self.lcd.lcd_puts(str(text), 1)
-        self.lcd.lcd_backlight(1)
+        self.lcd.write_string(text)
+        self.lcd.crlf()
     def clear(self):
-        self.lcd.lcd_clear()
+        self.lcd.crlf()
