befb5522333477a53db39fc71ba37b040678a156
David <davidzah6@seznam.cz>
2021-07-14 15:53:01 +0200

    new model

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import time
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
-from camera import get_frame
+#from camera import get_frame
 from ai_magic import Ai
-from lcd_driver import Display
+#from lcd_driver import Display
 
 ai = Ai('converted_savedmodel')
-lcd =  Display()
+
 for i in range(0,100):
-    image = get_frame()
-    print('frame get')
-    ai.predict_img(image)
+    #image = get_frame()
+    #print('frame get')
+    ai.predict_img([1,1])
     print(ai.get_prediction())
     lcd.clear()
     lcd.write(ai.get_prediction())
