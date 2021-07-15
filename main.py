660cc1d7d58a39bb39283ed4f31a25472dbed602
David <davidzah6@seznam.cz>
2021-07-15 13:39:38 +0200

    display support

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ import numpy as np
 #from camera import get_frame
 import camera
 from ai_magic import Ai
-#from lcd_driver import Display
+from lcd_driver import Display
 
 ai = Ai('converted_tflite')
-
+lcd = Display()
 for i in range(0,100):
     #image = Image.open('IMG_2060.jpg')
     image = camera.get_frame()
     print('frame get')
     ai.classify_image(image)
     print(ai.get_prediction())
-    #lcd.clear()
-    #lcd.write(ai.get_prediction())
-    #time.sleep(5)
+    lcd.clear()
+    lcd.write(ai.get_prediction())
+    time.sleep(5)
 
 
 
