4f957713764d6f21c0e6eff0eab049f4cc1317ee
David <davidzah6@seznam.cz>
2021-07-15 13:36:12 +0200

    rdy_for_test

--- ai_magic.py
+++ ai_magic.py
@@ -1,4 +1,6 @@
-import tensorflow.keras
+from PIL import Image
+import tensorflow as tf
+
 from PIL import Image, ImageOps
 import numpy as np
 
@@ -6,21 +8,10 @@ import numpy as np
 class Ai:
     def __init__(self,model_path):
         self.model_path = model_path
-        self.model = tensorflow.keras.models.load_model(model_path+'/model.savedmodel')
         self.__load_labels()
-
-
-    def predict_img(self,image):
-        np.set_printoptions(suppress=True)
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-        size = (224, 224)
-        image = ImageOps.fit(image, size, Image.ANTIALIAS)
-        image_array = np.asarray(image)
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-        data[0] = normalized_image_array
-        prediction = self.model.predict(data)
-        self.last_prediction = prediction
-        return prediction
+        self.interpreter = tf.lite.Interpreter(model_path+'/model_unquant.tflite')
+        self.interpreter.allocate_tensors()
+        _, self.height, self.width, _ = self.interpreter.get_input_details()[0]['shape']
 
     def get_labels(self):
         return self.lables
@@ -28,10 +19,31 @@ class Ai:
     def get_prediction(self):
         maximum = np.max(self.last_prediction)
         index_of_maximum = np.where(self.last_prediction == maximum)
-        return self.lables[index_of_maximum[1][0]] , maximum
+        return self.lables[index_of_maximum[0][0]] , maximum
 
     def __load_labels(self):
         with open(self.model_path+'/labels.txt', 'r') as f:
             labels = f.readlines()
             labels = list(map(lambda s: s.strip(), labels))
             self.lables = labels
+
+    def set_input_tensor(self, image):
+        tensor_index = self.interpreter.get_input_details()[0]['index']
+        input_tensor = self.interpreter.tensor(tensor_index)()[0]
+        input_tensor[:, :] = image
+
+    def classify_image(self, image, top_k=1):
+        image = ImageOps.fit(image, (self.height,self.width), Image.ANTIALIAS)
+        """Returns a sorted array of classification results."""
+        self.set_input_tensor(image)
+        self.interpreter.invoke()
+        output_details = self.interpreter.get_output_details()[0]
+        output = np.squeeze(self.interpreter.get_tensor(output_details['index']))
+
+        # If the model is quantized (uint8 data), then dequantize the results
+        if output_details['dtype'] == np.uint8:
+            scale, zero_point = output_details['quantization']
+            output = scale * (output - zero_point)
+
+        ordered = np.argpartition(-output, top_k)
+        self.last_prediction = output
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import time
 
-import tensorflow.keras
+
 from PIL import Image, ImageOps
 import numpy as np
-from camera import get_frame
+#from camera import get_frame
+import camera
 from ai_magic import Ai
-from lcd_driver import Display
+#from lcd_driver import Display
 
-ai = Ai('converted_savedmodel')
+ai = Ai('converted_tflite')
 
 for i in range(0,100):
-    image = get_frame()
+    #image = Image.open('IMG_2060.jpg')
+    image = camera.get_frame()
     print('frame get')
-    ai.predict_img([1,1])
+    ai.classify_image(image)
     print(ai.get_prediction())
-    lcd.clear()
-    lcd.write(ai.get_prediction())
-    time.sleep(5)
+    #lcd.clear()
+    #lcd.write(ai.get_prediction())
+    #time.sleep(5)
 
 
 
