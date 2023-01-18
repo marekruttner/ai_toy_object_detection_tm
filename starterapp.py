a0af7accb42349194a16c4f858285dfbdef760a9
TomasVOV <114948131+TomasVOV@users.noreply.github.com>
2023-01-18 22:26:53 +0100

    Add files via upload

--- starterapp.py
+++ starterapp.py
@@ -1,9 +1,5 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Color
-
+from kivy.uix.widget import Widget
 import os
 import runpy
 import webbrowser
@@ -11,69 +7,32 @@ import webbrowser
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-bgc = Color(0, 1, 0)
-black = [0, 0, 0, 0]
-
-class AiToySoftware(App):
-    def aBtn1Action(self, instance):
+class MainWidget(Widget):
+    def ICbtnAction(self):
         os.system("python "+DIR_PATH+'/main.py')
-
-    def aBtn2Action(self):
+        
+    def DCbtnAction(self):
         os.system("python "+DIR_PATH+'/dataset_capture/main.py')
 
-    def bBtn1Action(self, instance):
+    def MCbtnAction(self):
         os.system("python "+DIR_PATH+'/model_copy.py')
         
-    def bBtn2Action(self, instance):
+    def NVbtnAction(self):
         webbrowser.open('https://www.nvias.org/', new=1)
 
-    def build(self):
-        mainBox = BoxLayout(orientation="vertical")
+    def ODbtnAction(self):
+        pass
 
-        abboveBox = BoxLayout(orientation="horizontal")
+    def FMbtnAction(self):
+        pass
+    def SbtnAction(self):
+        exit()
 
-        white = [1, 1, 1, 1]
-        black = [0, 0, 0, 0]
 
-        bgc = Color(0, 1, 0)
 
-        aBtn1 = Button(text="TM image recognition",
-                        background_color=(255, 255, 255),
-                        color=(150, 0, 100, 1),
-                        font_size=32,
-                        size_hint=(0.7, 1))
-        aBtn1.bind(on_press=self.aBtn1Action)
-        aBtn2 = Button(text="Dataset taker",
-                        background_color=(255, 255, 255),
-                        color=(150, 0, 100, 1),
-                        font_size=32,
-                        size_hint=(0.7, 1))
+class StarterApp(App):
+    pass
 
-        abboveBox.add_widget(aBtn1)
-        abboveBox.add_widget(aBtn2)
 
-        belowBox = BoxLayout(orientation="horizontal")
-
-        bBtn1 = Button(text="Model copier",
-                        background_color=(255, 255, 255),
-                        color=(150, 0, 100, 1),
-                        font_size=32,
-                        size_hint=(0.7, 1))
-        bBtn2 = Button(text="Nvias.org",
-                        background_color=(255, 255, 255),
-                        color=(150, 0, 100, 1),
-                        font_size=32,
-                        size_hint=(0.7, 1))
-        bBtn2.bind(on_press=self.bBtn2Action)
-
-        belowBox.add_widget(bBtn1)
-        belowBox.add_widget(bBtn2)
-
-        mainBox.add_widget(abboveBox)
-        mainBox.add_widget(belowBox)
-
-        return mainBox
-
-main = AiToySoftware()
+main = StarterApp()
 main.run()
-
