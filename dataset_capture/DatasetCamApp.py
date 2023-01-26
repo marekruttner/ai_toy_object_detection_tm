eeb8ea00cad9176faac54acd093ee636aa32228b
TomasVOV <114948131+TomasVOV@users.noreply.github.com>
2023-01-26 16:00:57 +0100

    Add files via upload

--- dataset_capture/DatasetCamApp.py
+++ dataset_capture/DatasetCamApp.py
@@ -21,8 +21,9 @@ class P(FloatLayout):
         window.dismiss()
     
     def create_folder_btn(self):
-
+        global new_button
         global value
+
 
         if value == 1:
             new_button = Button(text=self.ids.textinput.text)
@@ -35,32 +36,56 @@ class P(FloatLayout):
             
 
         elif value == 2:
-            new_button1 = Button(text=self.ids.textinput.text)
-            self.main_widget.add_widget(new_button1) 
-            new_button1.pos = "250dp", "90dp"
-            new_button1.background_color = 255, 255, 255, 1
-            new_button1.color = 0, 0, 0, 1
-            value = 3
             
+            if new_button.text != self.ids.textinput.text:
+                new_button1 = Button(text=self.ids.textinput.text)
+                self.main_widget.add_widget(new_button1) 
+                new_button1.pos = "250dp", "90dp"
+                new_button1.background_color = 255, 255, 255, 1
+                new_button1.color = 0, 0, 0, 1
+                value = 3
+
+            else:
+                new_button1 = Button(text=self.ids.textinput.text + str(value))
+                self.main_widget.add_widget(new_button1) 
+                new_button1.pos = "250dp", "90dp"
+                new_button1.background_color = 255, 255, 255, 1
+                new_button1.color = 0, 0, 0, 1
+                value = 3 
 
         elif value == 3:
-            new_button2 = Button(text=self.ids.textinput.text)
-            self.main_widget.add_widget(new_button2) 
-            new_button2.pos = "150dp", "90dp"
-            new_button2.background_color = 255, 255, 255, 1
-            new_button2.color = 0, 0, 0, 1
-            value = 4
+            if new_button.text or new_button1.text == self.ids.textinput.text:
+                new_button2 = Button(text=self.ids.textinput.text + str(value))
+                self.main_widget.add_widget(new_button2) 
+                new_button2.pos = "150dp", "90dp"
+                new_button2.background_color = 255, 255, 255, 1
+                new_button2.color = 0, 0, 0, 1
+                value = 4
+            else: 
+                new_button2 = Button(text=self.ids.textinput.text)
+                self.main_widget.add_widget(new_button2) 
+                new_button2.pos = "150dp", "90dp"
+                new_button2.background_color = 255, 255, 255, 1
+                new_button2.color = 0, 0, 0, 1
+                value = 4
             
 
         elif value == 4:
-            new_button3 = Button(text=self.ids.textinput.text)
-            self.main_widget.add_widget(new_button3) 
-            new_button3.pos = "50dp", "90dp"
-            new_button3.background_color = 255, 255, 255, 1
-            new_button3.color = 0, 0, 0, 1
-            value = 5
-            
-
+            if  new_button.text or new_button1.text or new_button2.text == self.ids.textinput.text:
+                new_button3 = Button(text=self.ids.textinput.text + str(value))
+                self.main_widget.add_widget(new_button3) 
+                new_button3.pos = "50dp", "90dp"
+                new_button3.background_color = 255, 255, 255, 1
+                new_button3.color = 0, 0, 0, 1
+                value = 5
+            else:
+                new_button3 = Button(text=self.ids.textinput.text)
+                self.main_widget.add_widget(new_button3) 
+                new_button3.pos = "50dp", "90dp"
+                new_button3.background_color = 255, 255, 255, 1
+                new_button3.color = 0, 0, 0, 1
+                value = 5
+                
         elif value == 5:
             window.dismiss()
 
@@ -77,12 +102,11 @@ def show_popup(main_widget):
 
 def create_folder(self):
     folder_name = self.ids.textinput.text
-    os.makedirs(folder_name)
+    try:
+        os.makedirs(folder_name)
+    except:
+        os.makedirs(folder_name + str(value - 1))
 
-
-    
-    
     
 main = DatasetCamApp()
 main.run()
-
